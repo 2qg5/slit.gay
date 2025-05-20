e7b7e9b72330a95be25a467cb01e593af02a0138
2qg5 <turtleifys@gmail.com>
2025-05-20 04:00:02 -0600

    half update script.js

--- script.js
+++ script.js
@@ -8,28 +8,63 @@ const searchDatabase = {
         alternatives: [
             'https://peertube.social',
             'https://odysee.com',
-            'https://lbry.tv'
+            'https://lbry.tv',
+            'https://vid.puffyan.us',
+            'https://vid.piracy.moe'
         ]
     },
     'twitter': {
         alternatives: [
             'https://mastodon.social',
             'https://lemmy.ml',
-            'https://friendica.social'
+            'https://friendica.social',
+            'https://pleroma.social',
+            'https://gab.com'
         ]
     },
     'facebook': {
         alternatives: [
             'https://mastodon.social',
             'https://lemmy.ml',
-            'https://diaspora.software'
+            'https://diaspora.software',
+            'https://friendica.social',
+            'https://pleroma.social'
         ]
     },
     'google': {
         alternatives: [
             'https://duckduckgo.com',
             'https://searx.me',
-            'https://startpage.com'
+            'https://startpage.com',
+            'https://qwant.com',
+            'https://ecosia.org'
+        ]
+    },
+    'reddit': {
+        alternatives: [
+            'https://lemmy.ml',
+            'https://reddit.com',
+            'https://v.redd.it',
+            'https://voat.co',
+            'https://libreddit.net'
+        ]
+    },
+    'instagram': {
+        alternatives: [
+            'https://pixelfed.social',
+            'https://friendica.social',
+            'https://mastodon.social',
+            'https://lemmy.ml',
+            'https://gab.com'
+        ]
+    },
+    'tiktok': {
+        alternatives: [
+            'https://pixelfed.social',
+            'https://peertube.social',
+            'https://odysee.com',
+            'https://lbry.tv',
+            'https://vid.puffyan.us'
         ]
     }
 };
@@ -48,16 +83,26 @@ function displayResults(query) {
     }
 
     const alternatives = result.alternatives;
-
+    
+    // Add loading state
     resultsContainer.innerHTML = `
         <div class="result-item">
-            <div class="alternative-links">
-                ${alternatives.map(url => `
-                    <a href="${url}" class="alternative-link" target="_blank">${new URL(url).hostname}</a>
-                `).join('<br>')}
-            </div>
+            <p>Loading alternatives...</p>
         </div>
     `;
+
+    // Show results after a small delay
+    setTimeout(() => {
+        resultsContainer.innerHTML = `
+            <div class="result-item">
+                <div class="alternative-links">
+                    ${alternatives.map(url => `
+                        <a href="${url}" class="alternative-link" target="_blank">${new URL(url).hostname}</a>
+                    `).join('')}
+                </div>
+            </div>
+        `;
+    }, 300);
 }
 
 searchButton.addEventListener('click', () => {
